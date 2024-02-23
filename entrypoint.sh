2fcb2be3844a47597f6c55adfb3e43aa4a15a93b
Marcin Dabrowski <marcin.dabrowski@gft.com>
2024-02-23 17:51:32 +0100

    fix: Disable running sh as shell login
    When being run as login shell, PATH env variable
    misses /usr/local/go.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
-#!/bin/sh -l
+#!/bin/sh
+
+echo $PATH
 
 image_exists=$(cd /app && go run /app/ecr.go -region=$1 -repository-name=$2 -tag=$3 -access-key-id=$4 -secret-access-key=$5 )
 echo "image-exists=$image_exists" >> $GITHUB_OUTPUT
